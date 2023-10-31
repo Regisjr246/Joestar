955c2ba7f554d1fc87278791a18eaf4aaee95dec
Regisjr246 <regisjr246@gmail.com>
2023-10-31 15:38:46 -0300

    ss

--- src/components/CadastroClientes.tsx
+++ src/components/CadastroClientes.tsx
@@ -184,16 +184,16 @@ const CadastroCliente = () => {
                             <form onSubmit={cadastrarCliente} className='row g-3'>
                                 <div className='col-6'>
                                     <label htmlFor="nome" className='form-label'>Nome</label>
-                                    <input type="text" name='nome' className='form-control' required onChange={handleState} />
+                                    <input type="text" name='nome' className='form-control' required onChange={handleState} placeholder='ex: Torss' />
                                 </div>
                                 <div className='col-6'>
                                     <label htmlFor="email" className='form-label' >E-mail</label>
-                                    <input type="email" name='email' className='form-control' required onChange={handleState} />
+                                    <input type="email" name='email' className='form-control' required onChange={handleState}  placeholder='ex: xxxx@gmail.com'/>
 
                                 </div>
                                 <div className='col-4'>
                                     <label htmlFor="cpf" className='form-label'>CPF</label>
-                                    <input type="text" name='cpf' className='form-control' required onChange={handleState} />
+                                    <input type="text" name='cpf' className='form-control' required onChange={handleState} placeholder='ex: 11111111111'/>
                                 </div>
                                 <div className='col-4'>
                                     <label htmlFor="cpf" className='form-label'>Data de Nascimento</label>
@@ -202,45 +202,45 @@ const CadastroCliente = () => {
 
 
                                 <div className='col-4'>
-                                    <label htmlFor="celular" className='form-label'>Cep</label>
-                                    <input type="text" name='cep' className='form-control' required onBlur={findCep} onChange={handleState} />
+                                    <label htmlFor="cep" className='form-label'>Cep</label>
+                                    <input type="text" name='cep' className='form-control' required onBlur={findCep} onChange={handleState} placeholder='Só  numeros'/>
                                 </div>
                                 <div className='col-4'>
-                                    <label htmlFor="cpf" className='form-label'>Estado</label>
-                                    <input type="text" name='estado' value={estado} className='form-control' required onChange={handleState} />
+                                    <label htmlFor="estado" className='form-label'>Estado</label>
+                                    <input type="text" name='estado' value={estado} className='form-control' required onChange={handleState}placeholder='ex:SP' />
                                 </div>
 
                                 <div className='col-4'>
-                                    <label htmlFor="cpf" className='form-label'>Cidade</label>
-                                    <input type="text" value={localidade} id='localidade' name='localidade' className='form-control' required onChange={handleState} />
+                                    <label htmlFor="cidade" className='form-label'>Cidade</label>
+                                    <input type="text" value={localidade} id='localidade' name='localidade' className='form-control' required onChange={handleState}placeholder='Presidente Prudente' />
                                 </div>
                                 <div className='col-4'>
                                     <label htmlFor="celular" className='form-label'>Celular</label>
-                                    <input type="text" name='celular' className='form-control' required onChange={handleState} />
+                                    <input type="text" name='celular' className='form-control' required onChange={handleState} placeholder='ex: Apenas 11 numeros'/>
                                 </div>
                                 <div className='col-4'>
                                     <label htmlFor="celular" className='form-label'>Pais</label>
-                                    <input type="text" name='pais' className='form-control' required onChange={handleState} />
+                                    <input type="text" name='pais' className='form-control' required onChange={handleState} placeholder='ex: Brasil' />
                                 </div>
                                 <div className='col-4'>
-                                    <label htmlFor="celular" className='form-label'>Rua</label>
-                                    <input type="text" name='rua' className='form-control' required onChange={handleState} />
+                                    <label htmlFor="rua" className='form-label'>Rua</label>
+                                    <input type="text" name='rua' className='form-control' required onChange={handleState}placeholder='ex: Joao dragon' />
                                 </div>
                                 <div className='col-4'>
-                                    <label htmlFor="celular" className='form-label'>Numero</label>
-                                    <input type="text" name='numero' className='form-control' required onChange={handleState} />
+                                    <label htmlFor="numero" className='form-label'>Numero</label>
+                                    <input type="text" name='numero' className='form-control' required onChange={handleState} placeholder='ex: Apenas 11 numeros'/>
                                 </div>
                                 <div className='col-4'>
-                                    <label htmlFor="celular" className='form-label'>Bairro</label>
-                                    <input type="text" name='bairro' className='form-control' required onChange={handleState} />
+                                    <label htmlFor="bairro" className='form-label'>Bairro</label>
+                                    <input type="text" name='bairro' className='form-control' required onChange={handleState} placeholder='ex: renascer' />
                                 </div>
                                 <div className='col-4'>
-                                    <label htmlFor="celular" className='form-label'>Complemento</label>
-                                    <input type="text" name='complemento' className='form-control' required onChange={handleState} />
+                                    <label htmlFor="complemento" className='form-label'>Complemento</label>
+                                    <input type="text" name='complemento' className='form-control' required onChange={handleState} placeholder='ex: Quinta rua'/>
                                 </div>
                                 <div className='col-4'>
                                     <label htmlFor="password" className='form-label'>Senha</label>
-                                    <input type="password" name='password' className='form-control' required onChange={handleState} />
+                                    <input type="password" name='password' className='form-control' required onChange={handleState} placeholder='numeros, caracteres,simbolos' />
                                 </div>
                                 <div className='col-12'>
                                     <button type='submit' className='btn btn-success btn-sm'>Cadastrar</button>
